9a5b803123d18f28b7a9c8bc0d094bc2e76eee74
Dagobah <alejandrodagobah@gmail.com>
2023-02-01 11:21:41 -0500

    Generador de Barrio

--- public/js/city-builder.js
+++ public/js/city-builder.js
@@ -8,19 +8,20 @@ const loadAllModels = async function(_this) {
   }
 }
 
-async function modelInit(_this, nameModel, block, id) {
+async function modelInit(models3D, _this, nameModel, block, id, size) {
 
-  var models = await loadAllModels(_this)
   var position = block['pos_' + nameModel]
 
-  const house = new ExtendedObject3D() //Crear un nuevo objeto 3D de Enable
-  house.name = nameModel + '_' + id //Darle nombre
+  const object = new ExtendedObject3D() //Crear un nuevo objeto 3D de Enable
+  object.name = nameModel + '_' + id //Darle nombre
+  
 
-  house.add(models[nameModel].clone()) 
-    house.position.set(position.x, position.y, position.z) 
-    _this.add.existing(house)
+  object.add(models3D[nameModel].clone()) 
+  object.position.set(size.x + position.x, size.y + position.y, size.z + position.z) 
+    _this.add.existing(object)
 
-    house.traverse(child => {
+
+    object.traverse(child => {
     if(child.isMesh){
       child.castShadow = child.receiveShadow = false
       child.material.metalness = 0
@@ -39,21 +40,27 @@ async function modelInit(_this, nameModel, block, id) {
       }
     }
   })
+  console.log("ModelInit", nameModel, object.position)
 
-  return house.children[0].children[0].body
+  return object
 
 }
 
 
 var blocks = {
-  casaCampoResidencial_1: {
+  home_1: {
     pos_house: {x:0,y:0, z:0,},
     pos_tree: {x:8, y:0, z:5,},
     models: ['house','tree'],
 
   },
-  casaCampoResidencial_2:{},
-  casaCampoResidencial_3:{},
+  home_2: {
+    pos_house: {x:0,y:0, z:0,},
+    pos_tree: {x:8, y:0, z:2,},
+    models: ['house','tree'],
+
+  },
+  home_3:{},
 }
 
 function proceduralGeneration(){
@@ -69,100 +76,59 @@ function randomNumber() {
 
 async function createCity(map, _this) {
 
-    var _size = 0
+    var _size = {x:0, y:0, z:0}
     var i = 0
+    var models = await loadAllModels(_this)
 
-    builder.h(_this, _size, i)
-
-
-/*
     for (const itemRow of map) {
         for (const itemCol of itemRow) {
+            builder.creator(models, itemCol, _this, _size, i)
 
-            _size += await builder[itemCol](_this, _size, i)
+
+            _size.x += 20
+
             i++
+
         }
+
+       _size.z += 20
+       _size.x = 0
+
     }
-    */
+
     
 }
 
 var builder = {
-    h: async function(_this, _size, id) {
+    creator: function(models3D, item, _this, _size, id){
         
-        var primal
-        var list = []
 
-        for (let i = 0; i < blocks.casaCampoResidencial_1.models.length; i++) {
-          const model = blocks.casaCampoResidencial_1.models[i];
-          const block = blocks.casaCampoResidencial_1;
+        //Bucle para recorrer los modelos que componen un bloque
+        for (let i = 0; i < blocks[item].models.length; i++) {
+
+          const model = blocks[item].models[i];
+          const block = blocks[item];
+          modelInit(models3D,_this, model, block, id, _size)
           
+        }
 
-          var objetoEnable = modelInit(_this, model, block, id)
+        /*
+
+          var primal
+          var list = []
 
           if (i < 1) {
             primal = objetoEnable
           }else{
             list.push(objetoEnable)
           }
-          
-        }
+        
+          for (const item in list) {
+            _this.physics.add.constraints.lock(primal, item)
+          }
 
-        /*for (const item in list) {
-          _this.physics.add.constraints.lock(primal, item)
-        }
+
         */
 
     }
 }
-
-
-
-/*
-  MEDIR UN OBJETO 3D con THREEE
-  //Medir Casa
-  let bbox = new THREE.Box3().setFromObject(models[nameModel]);
-  let helper = new THREE.Box3Helper(bbox, new THREE.Color(0, 255, 0));
-  let size = bbox.getSize(new THREE.Vector3()); // HERE you get the size
-  house.add(helper);
-
-
-
-
-
-
-
-
-  
-  const tree = new ExtendedObject3D() 
-  tree.name = 'tree' + id
-
-  tree.add(models.tree.clone()) 
-    tree.position.set(8, 0, 5) 
-    _this.add.existing(tree)
-
-    tree.traverse(child => {
-    if(child.isMesh){
-      child.castShadow = child.receiveShadow = false
-      child.material.metalness = 0
-      child.material.roughness = 1
-
-      if(/SM/i.test(child.name)){
-        _this.physics.add.existing(child, {
-          shape: 'concave',
-          mass: 10,
-          collisionFlags: 1,
-          autoCenter: false
-        })
-
-        child.body.setAngularFactor(0,0,0) 
-        child.body.setLinearFactor(0,0,0) 
-      }
-    }
-  })
-
-
-  //_this.physics.add.constraints.lock(house.children[0].children[0].body, tree.children[0].children[0].body)
-
-
-  */
